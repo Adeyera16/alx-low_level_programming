ea53917b6b936073b573395be677358f28789f6f
Adeyera <asimolowoelizabeth@gmail.com>
2022-09-19 08:07:42 -0700

    This report, by its very length, defends itself against the risk of being read

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *_strlen - calculate the length of a string
+ *@s: A pointer to an int that will be changed
+ */
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (s[len] != '\0')
+	{
+		len++;
+	}
+	return (len);
+}
